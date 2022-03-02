632e13e67b23c353dc33cae6847cdcb135dd3068
Sumitskh <93589035+Sumitskh@users.noreply.github.com>
2022-03-02 12:56:19 +0530

    Add files via upload

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,1 +1,25 @@
 console.log("Hello World");
+
+    /* When the user clicks on the button, 
+    toggle between hiding and showing the dropdown content */
+    function myFunction() {
+        document.getElementById("myDropdown").classList.toggle("show");
+      }
+      // Modal
+      var modal = document.getElementById("myModalForAC");
+      var btnForAC = document.getElementById("btnforcat");
+      var Btn = document.getElementById("closeBtnForAC");
+      console.log(Btn)
+      btnForAC.onclick = function () {
+        modal.style.display = "block";
+      }
+      Btn.onclick = function () {
+        modal.style.display = "none";
+        // console.log("close button clicked")
+      }
+      window.onclick = function (event) {
+        if (event.target == modal) {
+          modal.style.display = "none";
+          // console.log("Outside windows clicked")
+        }
+      }
